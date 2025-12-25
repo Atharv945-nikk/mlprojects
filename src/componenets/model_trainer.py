9686af1fb1cad44926a2cff8b816ec8bd2dd8d40
Atharv Patil <atharvpatil49v@gmail.com>
2025-12-25 22:36:56 +0530

    hyperparemeter tuning

--- src/componenets/model_trainer.py
+++ src/componenets/model_trainer.py
@@ -14,8 +14,7 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import r2_score
 from xgboost import XGBRegressor
-
-
+from catboost import CatBoostRegressor
 
 from src.exception import CustomException
 from src.logger import logging
@@ -46,16 +45,72 @@ class Model_trainer:
                             "Linear Regression":LinearRegression(),
                             "Lasso":Lasso(),
                             "Ridge" :Ridge(),
-                            "Grident SearchCV":GradientBoostingRegressor(),
-                            "K-Neighbors Regressor": KNeighborsRegressor(),
-                            "DecisionTreeRegressor": DecisionTreeRegressor(),
+                            "Gradient Boosting Regressor":GradientBoostingRegressor(),
+                            "K-Neighbors Regressor":KNeighborsRegressor(),
+                            "DecisionTreeRegressor":DecisionTreeRegressor(),
                             "RandomForestRegressor":RandomForestRegressor(),
                             "XG Boot Regressor":XGBRegressor(),
                             "Ada Boost Regressor":AdaBoostRegressor(),
-                            "Cat Boost Regressor":CatBoostRegressor(verbose=False),
+                          #  "Cat Boost Regressor":CatBoostRegressor(verbose=False),
                     }
+            params = {
+                            "Linear Regression": {
+                                "fit_intercept": [True, False],
+                                "positive": [True, False]
+                            },
 
-            model_report:dict=evalution(X_train = X_train, X_test = X_test,y_train = y_train, y_test = y_test, models = models)   
+                            "Lasso": {
+                                "alpha": [0.0001, 0.001, 0.01, 0.1, 1.0],
+                                "fit_intercept": [True, False],
+                                "max_iter": [1000, 5000, 10000],
+                                "selection": ["cyclic", "random"]
+                            },
+
+                            "Ridge": {
+                                "alpha": [0.01, 0.1, 1.0, 10.0, 100.0],
+                                "fit_intercept": [True, False],
+                                "solver": ["auto", "svd", "cholesky", "lsqr"]
+                            },
+
+                            "Gradient Boosting Regressor": {
+                                "loss": ["squared_error", "absolute_error", "huber"],
+                                "learning_rate": [0.01, 0.05, 0.1],
+                                "n_estimators": [100, 200],
+                                "max_depth": [3, 5]
+                            },
+
+                            "K-Neighbors Regressor": {
+                                "n_neighbors": [3, 5, 7, 9],
+                                "weights": ["uniform", "distance"],
+                                "p": [1, 2]
+                            },
+
+                            "DecisionTreeRegressor": {
+                                "criterion": ["squared_error", "friedman_mse", "absolute_error"],
+                                "max_depth": [None, 10, 20],
+                                "min_samples_split": [2, 5, 10]
+                            },
+
+                            "RandomForestRegressor": {
+                                "n_estimators": [100, 200],
+                                "max_depth": [None, 20],
+                                "min_samples_split": [2, 5]
+                            },
+
+                            "XG Boot Regressor": {
+                                "n_estimators": [100, 200],
+                                "learning_rate": [0.05, 0.1],
+                                "max_depth": [3, 5]
+                            },
+
+                            "Ada Boost Regressor": {
+                                "n_estimators": [50, 100],
+                                "learning_rate": [0.05, 0.1]
+                            },
+                        }
+
+
+            model_report:dict=evalution(X_train = X_train, X_test = X_test,y_train = y_train, y_test = y_test, models = models,param = params)   
 
             #to find the best score form dict
             best_model_score = max(sorted((model_report.values())))
--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,7 @@ import pandas as pd
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 from src.logger import logging
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     
@@ -23,17 +23,26 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evalution(X_train,X_test,y_train,y_test,models):
+def evalution(X_train,X_test,y_train,y_test,models,param):
             
         try:
             report={}
             
             for i in range(len(list(models))):
                 model = list(models.values())[i]
+                paramg = param[list(models.keys())[i]]
 
+                grid = GridSearchCV(model,paramg,cv=3)
+
+                grid.fit(X_train,y_train)
+                 
+                model.set_params(**grid.best_params_)
                 model.fit(X_train,y_train)
+
+                # model.fit(X_train,y_train) train the model 
                  
                 y_train_pred = model.predict(X_train)
+                
                 y_test_pred = model.predict(X_test)
 
                 train_model_score  = r2_score(y_train,y_train_pred)
